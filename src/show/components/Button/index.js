da7f574ad4fac4f1dbe9684855f4b81aa7f664da
iamdavidpetric <david.petric99@gmail.com>
2022-06-23 13:07:50 +0300

    Resolve pull req issues

--- src/show/components/Button/index.js
+++ src/show/components/Button/index.js
@@ -1,16 +1,43 @@
-const Button = ({ className, color, onClick, children, size }) => {
+import PropTypes from 'prop-types';
+
+const Button = ({
+  className,
+  variant,
+  iconLeft,
+  iconRight,
+  label,
+  onClick
+}) => {
   const buttonColor = {
-    primary: 'bg-primary-500 text-primary-500'
+    primary: 'text-primary hover:text-primary-700',
+    secondary: 'text-white hover:text-secondary-200'
   };
 
   return (
     <button
-      className={`${buttonColor[color]} hover:text-white px-2 rounded ${className}`}
+      className={`${buttonColor[variant]} ${className} flex px-2 rounded transition-all duration-300`}
       onClick={onClick}>
-      {children}
-      {size}
+      {iconLeft}
+      {label}
+      {iconRight}
     </button>
   );
+};
+
+Button.defaultProps = {
+  className: '',
+  variant: 'primary',
+  onClick: () => {},
+  label: ''
+};
+
+Button.propTypes = {
+  className: PropTypes.string,
+  variant: PropTypes.string,
+  onClick: PropTypes.func,
+  label: PropTypes.string,
+  iconLeft: PropTypes.any,
+  iconRight: PropTypes.any
 };
 
 export default Button;
--- src/show/components/Navbar/index.js
+++ src/show/components/Navbar/index.js
@@ -1,28 +1,30 @@
-import Button from '../Button';
+import { Button } from '../';
 
-import { GiTreeGrowth } from 'react-icons/gi';
-import { MdOutlineExplore } from 'react-icons/md';
 import { BiSearch } from 'react-icons/bi';
-import { IoConstructOutline } from 'react-icons/io5';
 import { FiUnlock } from 'react-icons/fi';
 import { TbStairsUp } from 'react-icons/tb';
+import { GiTreeGrowth } from 'react-icons/gi';
+import { MdOutlineExplore } from 'react-icons/md';
+import { IoConstructOutline } from 'react-icons/io5';
 
 const Navbar = () => {
   return (
     <div className='flex flex-row h-12 justify-between bg-primary-50'>
       <div className='flex flex-row mx-2 items-center w-1/3'>
-        <div className='flex text-white transition duration-150 items-center'>
+        <div className='flex text-white transition duration-200 items-center'>
           <Button
-            className='flex'
-            onClick={() => console.log('button explore')}>
-            <MdOutlineExplore className='mx-1' size='1.5rem' /> explore
-          </Button>
+            variant='secondary'
+            iconLeft={<MdOutlineExplore className='mx-1' size='1.5rem' />}
+            label={'explore'}
+          />
         </div>
 
-        <div className='flex text-white transition duration-150 items-center'>
-          <Button className='flex' onClick={() => console.log('search btn')}>
-            <BiSearch className='mx-1' size='1.5rem' /> search
-          </Button>
+        <div className='flex transition duration-200 items-center'>
+          <Button
+            variant='secondary'
+            iconLeft={<BiSearch className='mx-1' size='1.5rem' />}
+            label={'search'}
+          />
         </div>
       </div>
 
@@ -31,27 +33,28 @@ const Navbar = () => {
       </div>
 
       <div className='flex flex-row items-center justify-end mx-2 w-1/3'>
-        <div className='flex text-white transition-all duration-300 items-center hover:px-2 hover:py-1'>
+        <div className='flex transition-all duration-300 items-center hover:px-2 hover:py-1'>
           <Button
-            className='flex'
-            onClick={() => console.log('how it works btn')}>
-            <IoConstructOutline className='mx-1' size='1.5rem' />
-            how it works
-          </Button>
+            variant='secondary'
+            iconLeft={<IoConstructOutline className='mx-1' size='1.5rem' />}
+            label={'how it works'}
+          />
         </div>
 
-        <div className='flex text-white transition-all duration-300 items-center hover:px-2 hover:py-1'>
-          <Button className='flex' onClick={() => console.log('sign in btn')}>
-            <FiUnlock className='mx-1' size='1.5rem' />
-            sign in
-          </Button>
+        <div className='flex transition-all duration-300 items-center hover:px-2 hover:py-1'>
+          <Button
+            variant='secondary'
+            iconLeft={<FiUnlock className='mx-1' size='1.5rem' />}
+            label={'sign in'}
+          />
         </div>
 
-        <div className='flex text-white border-2 border-primary-50 items-center hover:border-2 hover:rounded-full hover:border-white transition-all duration-300 hover:px-2 hover:py-1 '>
-          <Button className='flex' onClick={() => console.log('raise it btn')}>
-            <TbStairsUp className='px-0 mx-0 ' size='1.5rem' />
-            raise it
-          </Button>
+        <div className='flex  border-2 border-primary-50 items-center hover:border-2 hover:rounded-full hover:border-white transition-all duration-300 hover:px-2 hover:py-1 '>
+          <Button
+            variant='secondary'
+            iconLeft={<TbStairsUp className='px-0 mx-0 ' size='1.5rem' />}
+            label={'raise it'}
+          />
         </div>
       </div>
     </div>
