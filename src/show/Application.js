23eb24febe0c02a1ee5fbd29806276b9d7476cd1
iamdavidpetric <david.petric99@gmail.com>
2022-08-16 11:47:57 +0300

    Resolve PR2.0

--- src/show/Application.js
+++ src/show/Application.js
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import { Navbar } from 'show/components/';
-import routes from 'process/routes/index';
+import { Navbar } from 'show/components';
+import routes from 'process/routes';
 
 const Application = () => {
   return (
